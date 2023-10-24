f327735c429b283cbd9ddc426900d0077113ff16
yondav <yoni@yondav.us>
2023-10-23 23:41:46 -0400

    feat: adds routing for three pages wrapped in layout component
    / | /recipes | /recipes/:id

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+
+import MainLayout from './components/MainLayout';
+import { Home, Recipes, SingleRecipe } from './pages';
+
 function App() {
-  return <div className='bg-grapefruit-500'>app</div>;
+  return (
+    <Routes>
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path='recipes' element={<Recipes />} />
+        <Route path='recipes/:id' element={<SingleRecipe />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
