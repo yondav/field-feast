e7e56f0728011dee557c404fed458b989eae0c55
yondav <yoni@yondav.us>
2023-10-24 12:43:52 -0400

    feat(utils): adds numerical string type

--- src/utils/types.generic.ts
+++ src/utils/types.generic.ts
@@ -3,3 +3,7 @@ export type Nullable<T> = T | null | undefined;
 
 // Equivalent of keyof type, but for values.
 export type ValueOf<T> = T[keyof T];
+
+// Represents a numeric string type.
+export type NumericalString = `${number}`;
+
