ee49e1e88cf38678f02e5a76bcb84798a3920327
yondav <yoni@yondav.us>
2023-10-23 23:40:50 -0400

    feat(pages): adds components for pages
    Home | Recipes | SingleRecipes

---
+++ src/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as Home } from './pages.Home';
+export { default as Recipes } from './pages.Recipes';
+export { default as SingleRecipe } from './pages.SingleRecipe';
---
+++ src/pages/pages.SingleRecipe.tsx
@@ -0,0 +1,3 @@
+export default function SingleRecipe() {
+  return <>single recipe</>;
+}
